e4356bf4aab1c5ee5b0c9c4a291a1d43b240563a
Stan Krowinski <przemyslaw.krowinski@profinda.com>
2017-04-22 11:32:58 +0200

    Add basic controls and camera angles

---
+++ Assets/Scripts/AnimalController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	bool IsOutOfBounds(float x, float z) {
+    return (z < 0.0f || z > 4.0f);
+	}
+
+	void Move (float x, float z) {
+		float targetX = transform.position.x + x;
+		float targetZ = transform.position.z + z;
+		if(!IsOutOfBounds(targetX, targetZ)) {
+			Vector3 vector = new Vector3(x, 0.0f, z);
+			transform.Translate(vector);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp("w") || Input.GetKeyUp(KeyCode.UpArrow)) {
+			Move(-1.0f, 0.0f);
+		} else if(Input.GetKeyUp("s") || Input.GetKeyUp(KeyCode.DownArrow)) {
+			Move(1.0f, 0.0f);
+		} else if(Input.GetKeyUp("d") || Input.GetKeyUp(KeyCode.RightArrow)) {
+			Move(0.0f, 1.0f);
+		} else if(Input.GetKeyUp("a") || Input.GetKeyUp(KeyCode.LeftArrow)) {
+			Move(0.0f, -1.0f);
+		}
+	}
+}
---
+++ Assets/UVFreeShaders/README.txt
@@ -0,0 +1,8 @@
+UV-Free Triplanar Shader Pack
+by Andrew C. Wang (@andyman404)
+Version 2.0.2
+
+Please see documentation online at:
+http://idumpling.com/uvfree/v2.0.2
+
+You can delete the Sample folder after viewing.
